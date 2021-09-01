289af2cca0990ef1ba7296b4f9cce501b3d73fc1
David Ung <davidu@csedge.org>
2021-09-01 01:03:26 -0700

    Updated the theme
    Updated the theme of the website and changed the loook of cards and placements adding header

--- script.js
+++ script.js
@@ -16,22 +16,35 @@ let db = firebase.database();
 let calenderRef = db.ref('calender');
 calenderRef.once("value")
   .then(function(snapshot) {
+    let shoeTotal = 0;
+    let shoeCount = 0;
     snapshot.forEach(function(childSnapshot) {
+
       // childData will be the actual contents of the child
       var childData = childSnapshot.val();
 
           let card = `
                         <div class="card" style="width: 18rem;">
                         <div class="card-body">
-                          <h5 class="card-title">${childData.Date}</h5>
-                          <p class="card-text">${childData.Top}</p>
-                          <p class="card-text">${childData.Pants}</p>
-                          <p class="card-text">${childData.Shoes}</p>
+                          <div class="card-text">
+                          <h5 class="card-title"> Date: ${childData.Date}</h5>
+                          <p class="card-text">Tops: ${childData.Top}</p>
+                          <p class="card-text">Pants: ${childData.Pants}</p>
+                          <p class="card-text">Shoes: ${childData.Shoes}</p>
+                          <div>
                         </div>
                       </div>`
                       document.getElementById("cards").insertAdjacentHTML('beforeend', card);
+          if((childData.Shoes) == "Birkenstocks"){
+            shoeCount++;
+          }
+          shoeTotal++;
 
   });
+    let shoePercent = Math.floor((shoeCount/shoeTotal) * 100);
+    let shoeStat = `He wore Birkenstocks ${shoePercent}% of the time`;
+                      document.getElementById("shoeStat").insertAdjacentHTML('beforeend', shoeStat);
+
 });
 
 
@@ -40,17 +53,17 @@ calenderRef.once("value")
 //sumbitting new entry
 document.getElementById("submit").onclick = function(){
 //assigns value of the form to variables and then pushes it to the database
-    let date = document.getElementById("date").value;
+    let date = document.getElementById("date").value.replace(/[^0-9a-z]/gi, '');
     let tops = document.getElementById("tops").value;
     let bottoms = document.getElementById("bottoms").value;
     let shoes = document.getElementById("shoes").value;
+    let formatDate = document.getElementById("date").value;
      calenderRef.child(date).set({
-       Date : date,
+       Date : formatDate,
        Pants : bottoms,
        Shoes : shoes,
        Top : tops
    });
-
   //Alert to that has acknowledge it has been sumbited
   window.alert("form has been sumbited");
   window.location.reload()
